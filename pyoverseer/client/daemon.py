2ec3cc67f5c00e938497e5ccc4d9094964d46a49
Nathan Osman <admin@quickmediasolutions.com>
2014-06-08 20:50:03 -0700

    Implemented skeleton for client and server daemons.

---
+++ pyoverseer/client/daemon.py
@@ -0,0 +1,11 @@
+from .factory import PYOPFactory
+
+class ClientDaemon:
+    """Monitors running services."""
+    
+    def __init__(self):
+        """Initializes the client daemon."""
+    
+    def run(self):
+        """Enters the event loop of the client."""
+
---
+++ pyoverseer/server/daemon.py
@@ -0,0 +1,11 @@
+from .factory import PYOPFactory
+
+class ServerDaemon:
+    """Monitors client services."""
+    
+    def __init__(self):
+        """Initializes the server daemon."""
+    
+    def run(self):
+        """Enters the event loop of the server."""
+
