b0dc763fdbace2508bbce001e97e38396ddabecc
Nathan Osman <admin@quickmediasolutions.com>
2014-06-16 21:29:29 -0700

    Improved the organization of the Config class.

--- pyoverseer/config.py
+++ pyoverseer/config.py
@@ -1,40 +1,40 @@
+from collections import Mapping
 from json import load
 from os import path
 
 class Config:
     """Manages lookup and retrieval of configuration data."""
-    
+
     class ConfigMissingError(Exception):
         """Configuration file was not found."""
-    
+
     def __init__(self, filename, defaults={}):
         """Initialize the configuration."""
-        self._filename = filename
-        self._defaults = defaults
-        self.reload()
-    
+        self._config = defaults
+        self._update(self._config, self._load(filename))
+
+    def __contains__(self, key):
+        """Indicate whether the specified key exists."""
+        return key in self._config
+
+    def __getitem__(self, key):
+        """Return the value for the specified key."""
+        return self._config[key]
+
     def _find_config(self, filename):
         """Attempt to find the configuration file."""
         for directory in ['/etc/pyoverseer', '.']:
             if path.isfile(path.join(directory, filename)):
                 return path.join(directory, filename)
         raise ConfigMissingError('Configuration file %s does not exist.' % filename)
-    
-    def _lookup(self, _dict, key):
-        """Lookup a value in a dict by path."""
-        for component in key.split('/'):
-            _dict = _dict[component]
-        return _dict
-    
-    def reload(self):
-        """Reload the configuration from disk."""
-        with open(self._find_config(self._filename), 'r') as f:
-            self._config = load(f)
-    
-    def get(self, key):
-        """Retrieve the value for the specified key."""
-        try:
-            return self._lookup(self._config, key)
-        except KeyError:
-            return self._lookup(self._defaults, key)
 
+    def _load(self, filename):
+        """Load the configuration from disk."""
+        with open(self._find_config(filename), 'r') as f:
+            return load(f)
+
+    def _update(self, d, u):
+        """Recursive update from http://stackoverflow.com/a/3233356/193619."""
+        for k, v in u.iteritems():
+            d[k] = self._update(d.get(k, {}), v) if isinstance(v, Mapping) else u[k]
+        return d
